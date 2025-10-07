6bdea9f4c7d690df298d879245d74f4f20f6f7f3
Christine-indigo21 <christine.carillo@indigo21.com>
2025-10-07 19:43:47 +0100

    Inventory

---
+++ resources/views/layouts/body/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer footer-transparent d-print-none">
+    <div class="container-xl">
+        <div class="row text-center align-items-center flex-row-reverse">
+        
+            <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item">
+                        Copyright &copy; <script>document.write(new Date().getFullYear())</script>
+                        <a href="www.indigo21.com" class="link-secondary">Indigo 21</a>.
+                        All rights reserved.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
